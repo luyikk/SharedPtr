4057d219090b8a5c832477dd8e01727fcb6b9882
yi lu <luyikk@126.com>
2021-03-19 11:04:15 +0800

    1

---
+++ examples/main.rs
@@ -0,0 +1,36 @@
+use sharedptr::SharedPtr;
+
+struct Foo(String);
+
+fn main() {
+    let mut ptr = SharedPtr::<Foo>::zeroed();
+    assert_eq!(ptr.is_null(), true);
+
+    ptr.write(Foo("122".to_string()));
+
+    println!("{}", ptr.0);
+
+    let mut ptr2 = ptr.clone();
+    println!("{}", ptr2.0);
+
+    ptr2.set_null();
+    println!("{}", ptr.0);
+
+    if ptr2.weak().is_none() {
+        println!("ptr 2 weak fail")
+    }
+
+    let wk = ptr.weak().unwrap();
+    println!("{}", wk.upgrade().unwrap().0);
+
+    drop(ptr);
+
+    // if let Some(rc) = ptr.assume_init() {
+    //     println!("{}", rc.0);
+    // }
+
+    if wk.upgrade().is_none(){
+        println!("ptr is drop");
+    }
+
+}
